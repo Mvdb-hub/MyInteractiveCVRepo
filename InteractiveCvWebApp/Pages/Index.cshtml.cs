a0f43433ef9aef6788b66cfddaf7c52f92c98e75
Mvdb-hub <mvdbroucke@outlook.be>
2020-04-13 18:07:15 +0200

    Created IndexPageModel with test html

--- InteractiveCvWebApp/Pages/Index.cshtml.cs
+++ InteractiveCvWebApp/Pages/Index.cshtml.cs
@@ -2,24 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InteractiveCvAspCore.Data;
+using InteractiveCvAspCore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace InteractiveCvWebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly InteractiveCvContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(InteractiveCvContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
 
+        public Person FirstPerson { get; set; }
+        public int PersonAge { get; set; }
+
+        public IEnumerable<AbilityCategory> AbilityCategories { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            FirstPerson = await _context.Person
+                .Include(a => a.Abilities)
+                .ThenInclude(c => c.Category).FirstOrDefaultAsync();
+
+            FirstPerson.Educations = await _context.PersonEducation
+                .Where(e => e.PersonID == FirstPerson.ID).OrderBy(e => e.StartDate).ToListAsync();
+
+            FirstPerson.Experiences = await _context.PersonExperience
+                .Where(e => e.PersonID == FirstPerson.ID).OrderBy(e => e.StartDate).ToListAsync();
+
+            AbilityCategories = await _context.AbilityCategory.ToListAsync();
+
+            PersonAge = 34; //create calculate age function in Person Model
+            if (FirstPerson == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
         }
     }
 }
